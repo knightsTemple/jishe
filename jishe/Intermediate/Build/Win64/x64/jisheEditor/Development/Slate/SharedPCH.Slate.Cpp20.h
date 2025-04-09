58800060a53f1f88bdbc27f4ba6cb3016b6f6e8f
yuyixiaolan <143372967+yuyixiaolan@users.noreply.github.com>
2025-04-09 10:54:50 +0800

    Revert "UI drawiings"
    This reverts commit fd26f7c84dfcf344053e5bd1bdbe88e989d9e87b.

--- jishe/Intermediate/Build/Win64/x64/jisheEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
+++ jishe/Intermediate/Build/Win64/x64/jisheEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
@@ -1,3 +1,3 @@
 // PCH for Runtime/Slate/Public/SlateSharedPCH.h
-#include "E:/01UE project/jishe/jishe/jishe/Intermediate/Build/Win64/x64/jisheEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
+#include "F:/Unreal/jishe/jishe/Intermediate/Build/Win64/x64/jisheEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
 #include "Runtime/Slate/Public/SlateSharedPCH.h"
